55447c33fa2e2cbb153498387db6b587bed7d9a5
habeebweeb <habeebweeb@habeeb.weeb>
2020-08-08 18:56:08 -0700

    Add keyboard enter event for TextField

--- COM3D2.MeidoPhotoStudio.Plugin/MeidoPhotoStudio/GUI/Controls/TextField.cs
+++ COM3D2.MeidoPhotoStudio.Plugin/MeidoPhotoStudio/GUI/Controls/TextField.cs
@@ -1,14 +1,20 @@
+using System;
 using UnityEngine;
 
 namespace COM3D2.MeidoPhotoStudio.Plugin
 {
     internal class TextField : BaseControl
     {
+        private static int textFieldID = 961;
+        private static int ID => ++textFieldID;
+        private readonly string controlName = $"textField{ID}";
         public string Value { get; set; } = string.Empty;
         public void Draw(GUIStyle textFieldStyle, params GUILayoutOption[] layoutOptions)
         {
             if (!Visible) return;
+            GUI.SetNextControlName(controlName);
             Value = GUILayout.TextField(Value, textFieldStyle, layoutOptions);
+            if (Event.current.isKey && Event.current.keyCode == KeyCode.Return) OnControlEvent(EventArgs.Empty);
         }
 
         public override void Draw(params GUILayoutOption[] layoutOptions)
