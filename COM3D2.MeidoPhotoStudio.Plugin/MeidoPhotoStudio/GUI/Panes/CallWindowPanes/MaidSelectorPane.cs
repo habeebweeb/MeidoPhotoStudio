64b8321295df31ad6d571398b26555d6bdf9706b
habeebweeb <habeebweeb@habeeb.weeb>
2020-10-06 10:51:43 -0700

    Change layout of clear and call button
    Both buttons are now next to each other but the clear button is now way
    smaller. Reduces chances of accidentally clicking clear.

--- COM3D2.MeidoPhotoStudio.Plugin/MeidoPhotoStudio/GUI/Panes/CallWindowPanes/MaidSelectorPane.cs
+++ COM3D2.MeidoPhotoStudio.Plugin/MeidoPhotoStudio/GUI/Panes/CallWindowPanes/MaidSelectorPane.cs
@@ -26,8 +26,10 @@ namespace COM3D2.MeidoPhotoStudio.Plugin
 
         public override void Draw()
         {
-            clearMaidsButton.Draw();
+            GUILayout.BeginHorizontal();
+            clearMaidsButton.Draw(GUILayout.ExpandWidth(false));
             callMaidsButton.Draw();
+            GUILayout.EndHorizontal();
 
             GUIStyle labelStyle = new GUIStyle(GUI.skin.label) { fontSize = 14 };
             GUIStyle selectLabelStyle = new GUIStyle(labelStyle);
@@ -41,8 +43,8 @@ namespace COM3D2.MeidoPhotoStudio.Plugin
             float buttonWidth = windowRect.width - 30f;
             const float buttonHeight = 85f;
 
-            Rect positionRect = new Rect(5, 115f, windowRect.width - 10f, windowHeight - 150f);
-            Rect viewRect = new Rect(0, 0, buttonWidth, (buttonHeight * meidoManager.Meidos.Length) + 5f);
+            Rect positionRect = new Rect(5f, 90f, windowRect.width - 10f, windowHeight - 125f);
+            Rect viewRect = new Rect(0f, 0f, buttonWidth, (buttonHeight * meidoManager.Meidos.Length) + 5f);
             maidListScrollPos = GUI.BeginScrollView(positionRect, maidListScrollPos, viewRect);
 
             for (int i = 0; i < meidoManager.Meidos.Length; i++)
