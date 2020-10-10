2544c98e1a00f0dd486e651493ad42c1b4763ccf
habeebweeb <habeebweeb@habeeb.weeb>
2020-10-09 18:33:05 -0700

    Set DefaultPosition for dogu drag handle
    Some stages like the salon stage in CM3D2 have a default position that
    isn't Vector3.zero.

--- COM3D2.MeidoPhotoStudio.Plugin/MeidoPhotoStudio/DragPoint/DragPointOther.cs
+++ COM3D2.MeidoPhotoStudio.Plugin/MeidoPhotoStudio/DragPoint/DragPointOther.cs
@@ -32,6 +32,12 @@ namespace COM3D2.MeidoPhotoStudio.Plugin
 
     public class DragPointBG : DragPointGeneral
     {
+        public override void Set(Transform myObject)
+        {
+            base.Set(myObject);
+            DefaultPosition = myObject.position;
+        }
+
         protected override void ApplyDragType()
         {
             ApplyProperties(Transforming, Transforming, Rotating);
