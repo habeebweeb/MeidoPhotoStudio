28522f4716cdc55fe4c293e7b4d30b5b2e8d6c52
habeebweeb <habeebweeb@habeeb.weeb>
2021-06-20 11:49:49 -0700

    Make UI smaller and draggable

--- src/MeidoPhotoStudio.Converter/UI.cs
+++ src/MeidoPhotoStudio.Converter/UI.cs
@@ -20,8 +20,8 @@ namespace MeidoPhotoStudio.Converter
             if (!Visible)
                 return;
 
-            windowRect.width = 300f;
-            windowRect.height = 200f;
+            windowRect.width = 230f;
+            windowRect.height = 100f;
             windowRect.x = Mathf.Clamp(windowRect.x, 0, Screen.width - windowRect.width);
             windowRect.y = Mathf.Clamp(windowRect.y, 0, Screen.height - windowRect.height);
             windowRect = GUI.Window(WindowID, windowRect, GUIFunc, WindowTitle);
@@ -33,6 +33,8 @@ namespace MeidoPhotoStudio.Converter
 
             if (GUILayout.Button("Convert"))
                 core.Convert();
+
+            GUI.DragWindow();
         }
     }
 }
