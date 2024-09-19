d9b299080edd8d424de082eccb8f4a09b1012cc4
habeebweeb <40900143+habeebweeb@users.noreply.github.com>
2024-09-18 18:31:15 -0700

    Add placeholder to text field

--- src/MeidoPhotoStudio.Plugin/Framework/UI/Legacy/TextField.cs
+++ src/MeidoPhotoStudio.Plugin/Framework/UI/Legacy/TextField.cs
@@ -6,6 +6,10 @@ public class TextField : BaseControl
 
     private readonly string controlName = $"textField{ID}";
 
+    private GUIContent placeholderContent = GUIContent.none;
+    private bool hasPlacholder = false;
+    private string placeholder = string.Empty;
+
     public event EventHandler GainedFocus;
 
     public event EventHandler LostFocus;
@@ -16,19 +20,64 @@ public class TextField : BaseControl
 
     public string Value { get; set; } = string.Empty;
 
+    public string Placeholder
+    {
+        get => placeholder;
+        set
+        {
+            if (string.Equals(placeholder, value, StringComparison.Ordinal))
+                return;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                hasPlacholder = false;
+                placeholder = string.Empty;
+                placeholderContent = GUIContent.none;
+            }
+            else
+            {
+                hasPlacholder = true;
+                placeholder = value;
+                placeholderContent = new(placeholder);
+            }
+        }
+    }
+
     public bool HasFocus { get; private set; }
+
+    private static LazyStyle PlaceholderStyle { get; } = new(
+        13,
+        () => new(GUI.skin.label)
+        {
+            padding = new(5, 5, 0, 0),
+            alignment = TextAnchor.MiddleLeft,
+            normal =
+            {
+                textColor = new(1f, 1f, 1f, 0.6f),
+            },
+        });
 
     private static int ID =>
         ++textFieldID;
 
     public override void Draw(params GUILayoutOption[] layoutOptions) =>
-        Draw(Style, layoutOptions);
+        Draw(Style, PlaceholderStyle, layoutOptions);
 
-    public void Draw(GUIStyle textFieldStyle, params GUILayoutOption[] layoutOptions)
+    public void Draw(GUIStyle textFieldStyle, params GUILayoutOption[] layoutOptions) =>
+        Draw(textFieldStyle, PlaceholderStyle, layoutOptions);
+
+    public void Draw(GUIStyle textFieldStyle, GUIStyle placeholderStyle, params GUILayoutOption[] layoutOptions)
     {
         GUI.SetNextControlName(controlName);
 
         var value = GUILayout.TextField(Value, textFieldStyle, layoutOptions);
+
+        if (hasPlacholder && value.Length is 0)
+        {
+            var textFieldRect = GUILayoutUtility.GetLastRect();
+
+            GUI.Label(textFieldRect, placeholderContent, placeholderStyle);
+        }
 
         if (!string.Equals(value, Value, StringComparison.Ordinal))
         {
