cb7b56acbfa25b4954985a288ed55acda1174f56
habeebweeb <40900143+habeebweeb@users.noreply.github.com>
2025-05-21 16:51:56 -0700

    Change default drag handle colours

--- src/MeidoPhotoStudio.Plugin/Core/Configuration/DragHandleConfiguration.cs
+++ src/MeidoPhotoStudio.Plugin/Core/Configuration/DragHandleConfiguration.cs
@@ -17,16 +17,16 @@ public class DragHandleConfiguration
         AutomaticTabSelection = this.configFile.Bind(GeneralSection, "Automatically Change Tab On Interaction", false);
         SmallTransformCube = this.configFile.Bind(GeneralSection, "Small Transform Drag Handles", false);
         CharacterTransformCube = this.configFile.Bind(GeneralSection, "Character Transform Drag Handle", false);
-        UpperLimbDragHandleColour = this.configFile.Bind(ColourSection, "Upper Limb Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        MiddleLimbDragHandleColour = this.configFile.Bind(ColourSection, "Middle Limb Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        LowerLimbDragHandleColour = this.configFile.Bind(ColourSection, "Lower Limb Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        SpineDragHandleColour = this.configFile.Bind(ColourSection, "Spine Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        RootDragHandleColour = this.configFile.Bind(ColourSection, "Root Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        BaseDigitJointColour = this.configFile.Bind(ColourSection, "Base Digit Joint Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        MiddleDigitJointColour = this.configFile.Bind(ColourSection, "Middle Digit Joint Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        TipDigitJointColour = this.configFile.Bind(ColourSection, "Tip Digit Joint Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        ClothingDragHandleColour = this.configFile.Bind(ColourSection, "Clothing Gravity Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
-        HairDragHandleColour = this.configFile.Bind(ColourSection, "Hair Gravity Drag Handle Colour", new Color(0f, 0f, 0f, 0.75f));
+        UpperLimbDragHandleColour = this.configFile.Bind(ColourSection, "Upper Limb Drag Handle Colour", new Color(0.9f, 0.17f, 0f, 0.7f));
+        MiddleLimbDragHandleColour = this.configFile.Bind(ColourSection, "Middle Limb Drag Handle Colour", new Color(0.01f, 0.85f, 0.39f, 0.7f));
+        LowerLimbDragHandleColour = this.configFile.Bind(ColourSection, "Lower Limb Drag Handle Colour", new Color(0.05f, 0f, 0.86f, 0.7f));
+        SpineDragHandleColour = this.configFile.Bind(ColourSection, "Spine Drag Handle Colour", new Color(0.45f, 0f, 0.8f, 0.6f));
+        RootDragHandleColour = this.configFile.Bind(ColourSection, "Root Drag Handle Colour", new Color(0f, 0.84f, 0.6f, 0.7f));
+        BaseDigitJointColour = this.configFile.Bind(ColourSection, "Base Digit Joint Drag Handle Colour", new Color(0.9f, 0.1f, 0f, 0.3f));
+        MiddleDigitJointColour = this.configFile.Bind(ColourSection, "Middle Digit Joint Drag Handle Colour", new Color(0f, 0.9f, 0.07f, 0.3f));
+        TipDigitJointColour = this.configFile.Bind(ColourSection, "Tip Digit Joint Drag Handle Colour", new Color(0.19f, 0.05f, 0.96f, 0.3f));
+        ClothingDragHandleColour = this.configFile.Bind(ColourSection, "Clothing Gravity Drag Handle Colour", new Color(0.86f, 0.75f, 0.3f, 0.7f));
+        HairDragHandleColour = this.configFile.Bind(ColourSection, "Hair Gravity Drag Handle Colour", new Color(0f, 0.47f, 0.19f, 0.7f));
     }
 
     public ConfigEntry<bool> AutomaticSelection { get; }
